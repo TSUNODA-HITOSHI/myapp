5e91623c05a352920a840f1d31ebf221bc4dcb51
角田仁志 <shinsui889@hotmail.com>
2021-11-11 01:32:28 +0000

    2021.11.10まで

---
+++ resources/views/admin/attend/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('title','出退勤')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>出退勤</h2>
+            </div>
+        </div>
+    </div>
+@endsection
